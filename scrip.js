13143e8af8f4839966e23b2ad7b3854121e06476
Carolina Case <69168196+carolinacase@users.noreply.github.com>
2024-11-14 14:28:48 -0300

    fixed stuff

--- scrip.js
+++ scrip.js
@@ -27,3 +27,16 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove('.scrolled');  // Remueve la clase 'scrolled' cuando el scroll es menor a 50px
     }
 });
+
+function copyEmail() {
+    const email = document.getElementById("email-display").textContent;
+    navigator.clipboard.writeText(email).then(() => {
+      alert("Email copiado al portapapeles");
+    });
+  }
+  
+  function sendEmail() {
+    const email = document.getElementById("email-display").textContent;
+    window.location.href = `mailto:${email}?subject=Contacto%20desde%20tu%20portafolio`;
+  }
+  
